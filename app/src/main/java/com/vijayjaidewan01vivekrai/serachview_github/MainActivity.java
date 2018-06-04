1635556cae6025de4932eaac4dbf6f52f6c335e9
Vivek Rai <vijayjaidewan01@gmail.com>
2018-06-04 11:46:36 +0530

    Initial commit

---
+++ app/src/main/java/com/vijayjaidewan01vivekrai/serachview_github/MainActivity.java
@@ -0,0 +1,67 @@
+package com.vijayjaidewan01vivekrai.serachview_github;
+
+import android.annotation.TargetApi;
+import android.app.SearchManager;
+import android.content.Context;
+import android.database.MatrixCursor;
+import android.os.Build;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.SearchView;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+   ListView listView;
+   ArrayList<String> list;
+   ArrayAdapter<String> adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        listView = findViewById(R.id.listView);
+        list = new ArrayList<>();
+        list.addAll(Arrays.asList(getResources().getStringArray(R.array.my_array)));
+        adapter = new ArrayAdapter<String>(MainActivity.this, android.R.layout.simple_list_item_1, list);
+        listView.setAdapter(adapter);
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.search_menu, menu);
+        MenuItem item = menu.findItem(R.id.search_food);
+        android.widget.SearchView searchView = (android.widget.SearchView) item.getActionView();
+
+        searchView.setOnQueryTextListener(new android.widget.SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String s) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String s) {
+                adapter.getFilter().filter(s);
+                return false;
+            }
+        });
+
+        return super.onCreateOptionsMenu(menu);
+
+
+    }
+}
